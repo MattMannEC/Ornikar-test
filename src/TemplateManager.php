b35214fc6295d0d769ce97718412199af8deddc6
mattmannec <mattmannec@gmail.com>
2021-09-09 18:18:18 +0200

    Loop through template variables

--- src/TemplateManager.php
+++ src/TemplateManager.php
@@ -34,47 +34,22 @@ class TemplateManager
 
     private function computeText($text, array $data)
     {
-        if ($this->lesson)
-        {
-            $text = str_replace('[instructor_link]',  'instructors/' . $this->lesson->instructor->id .'-'.urlencode($this->lesson->instructor->firstname), $text);
+        $templateVars = [
+            'instructor_link' => $this->lesson->instructor->getLink(),
+            'lesson:summary_html' => Lesson::renderHtml($this->lesson),
+            'lesson:summary' => Lesson::renderText($this->lesson),
+            'lesson:instructor_name' => $this->lesson->instructor->firstname,
+            'lesson:meeting_point' => $this->lesson->name,
+            'lesson:start_date' => $this->lesson->start_time->format('d/m/Y'),
+            'lesson:start_time' => $this->lesson->start_time->format('H:i'),
+            'lesson:end_time' => $this->lesson->end_time->format('H:i'),
+            'user:first_name' => strtolower($this->user->firstname),
+        ];
 
-            $containsSummaryHtml = strpos($text, '[lesson:summary_html]');
-            $containsSummary     = strpos($text, '[lesson:summary]');
-
-            if ($containsSummaryHtml !== false) {
-                $text = str_replace(
-                    '[lesson:summary_html]',
-                    Lesson::renderHtml($this->lesson),
-                    $text
-                );
-            }
-
-            if ($containsSummary !== false) {
-                $text = str_replace(
-                    '[lesson:summary]',
-                    Lesson::renderText($this->lesson),
-                    $text
-                );
-            }
-
-        $text = str_replace('[lesson:instructor_name]',$this->lesson->instructor->firstname, $text);
+        foreach ($templateVars as $wildcard => $value) {
+            str_replace($wildcard, $value, $text);
         }
-
-        if ($this->lesson->meetingPointId) {
-                $text = str_replace('[lesson:meeting_point]', $this->lesson->name, $text);
-        }
-
-        $text = str_replace('[lesson:start_date]', $this->lesson->start_time->format('d/m/Y'), $text);
-        $text = str_replace('[lesson:start_time]', $this->lesson->start_time->format('H:i'), $text);
-        $text = str_replace('[lesson:end_time]', $this->lesson->end_time->format('H:i'), $text);
-
-            if ($data['instructor']  instanceof Instructor)
-                $text = str_replace('[instructor_link]',  'instructors/' . $data['instructor']->id .'-'.urlencode($data['instructor']->firstname), $text);
-            else
-                $text = str_replace('[instructor_link]', '', $text);
-
-        $text = str_replace('[user:first_name]', strtolower($this->user->firstname), $text);
-
+            
         return $text;
     }
 }
